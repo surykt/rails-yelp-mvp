0943d4b68f64dc7a608e1d2928a7238318186244
Carlos Alan <cardosocarlosalan@gmail.com>
2022-08-17 16:08:10 -0300

    create two models validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  Rails.application.routes.draw do
+    resources :restaurants
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+end
+
+Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: Faker::Restaurant.name,
+  address: Faker::Address.street_address,
+  phone_number: Faker::Number.leading_zero_number(digits: 11),
+  category: Restaurant::CATEGORIES.sample}
+pizza_east =  {name: "Pizza East", address: Faker::Address.street_address,
+  phone_number: Faker::Number.leading_zero_number(digits: 11),
+  category: Restaurant::CATEGORIES.sample}
+
+[dishoom, pizza_east].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
